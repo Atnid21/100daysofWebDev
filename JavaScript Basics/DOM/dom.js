f70b9a6cf64909915b674e022a40eb1958653a2b
Atharva Nidhonkar <atharvanidhonkar10@gmail.com>
2023-11-21 20:33:23 +0530

    added dom

---
+++ JavaScript Basics/DOM/dom.js
@@ -0,0 +1,34 @@
+// const a = document.querySelector("h1") // class: .h1
+// id: #h1 
+
+// console.log(a);
+// a.innerHTML = "New"
+
+// a.style.color='red'
+
+// a.style.backgroundColor = "black"
+
+// a.addEventListener("click",function(){
+//     a.innerHTML="Ho gaya click";
+//     a.style.color="black";
+//     a.style.backgroundColor="blue"
+// })
+
+
+var bulb = document.querySelector("#bulb");
+
+var button=document.querySelector("button");
+
+var flag=0;
+
+button.addEventListener("click",function(){
+    if(flag==0){
+        bulb.style.backgroundColor="yellow";
+        flag=1;
+    }else{
+        bulb.style.backgroundColor="transparent";
+        flag=0;
+}
+
+})
+
